58c96ee68f969d65fffec6e006e7f2652b64658f
Nitin Sahu <ns0779687@gmail.com>
2025-06-24 12:32:19 +0530

    added get notes api route

--- frontend/src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-    const token = localStorage.getItem("token");
-    return token && token.length > 10 ? children : <Navigate to="/login" />;    
-};
-
-export default PrivateRoute;
